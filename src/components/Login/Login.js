9a6b8ef3639eaac4c744933c2d2e4b026833069c
SO HUI <haejuuun925@gmail.com>
2023-10-14 19:44:20 +0900

    feat: useReducer을 이용하여 password 상태 및 데이터 업데이트

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -19,49 +19,73 @@ const emailReducer = (state, action) => {
     return { value: '', isValid: false };
 };
 
+const passwordReducer = (state, action) => {
+    if(action.type === 'USER_INPUT') {
+        // USER_INPUT 액션 받을 때마다 value, isValid 모두 업데이트.
+        return { value: action.val, isValid: action.val.trim().length > 6 };
+    }
+    if(action.type === 'INPUT_BLUR') {
+        // state.value : 이메일에 입력한 최신 값에 접근하기 위함.
+        return { value: state.value, isValid: state.value.trim().length > 6 };
+    }
+    // 위 조건 제외, 다른 모든 액션에 대해서는 아래 기본 state 반환.
+    return { value: '', isValid: false };
+};
+
 const Login = (props) => {
     // const [enteredEmail, setEnteredEmail] = useState('');
     // const [emailIsValid, setEmailIsValid] = useState();
-    const [enteredPassword, setEnteredPassword] = useState('');
-    const [passwordIsValid, setPasswordIsValid] = useState();
+    // const [enteredPassword, setEnteredPassword] = useState('');
+    // const [passwordIsValid, setPasswordIsValid] = useState();
     const [formIsValid, setFormIsValid] = useState(false);
 
     const [emailState, dispatchEmail] = useReducer(emailReducer, { 
         value: '', 
         isValid: false,
     });
+
+    const [passwordState, dispatchPassword] = useReducer(passwordReducer, { 
+        value: '', 
+        isValid: false,
+    });
+
+    // inValid의 속성을 꺼내 상수인 emailIsValid, passwordIsValid에 저장
+                  // 별칭 할당
+    const { isValid: emailIsValid } = emailState;
+    const { isValid: passwordIsValid } = passwordState;
+    // 위 디스턱처링은 useEffect가 불필요하게 실행되는 것을 막아주기 때문에
+    // 값이 유효하다면 유효성 검사를 다시 실행 X
     
     // 1. 모든 로그인 컴포넌트 함수 실행 후 useEffect 재실행
     // 2. setFormIsValid, enteredEmail 혹은 enteredPassword가 마지막 컴포넌트 렌더링 주기에서 변경된 경우에만 실행
     // 3. state 업데이트 함수는 기본적으로 리액트에 의해 절대 변경되지 않도록 보장되기 때문에 setFormIsValid 생략 가능
-    // useEffect(() => {
-    //     const identifier = setTimeout(() => {
-    //         console.log('checking form validity');
-    //         setFormIsValid(
-    //             enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    //         );
-    //     }, 500);  // 500밀리초 후에만 해당 작업 수행
+    useEffect(() => {
+        const identifier = setTimeout(() => {
+            console.log('checking form validity');
+            setFormIsValid(
+                emailIsValid && passwordIsValid
+            );
+        }, 500);  // 500밀리초 후에만 해당 작업 수행
 
-    //     return () => {
-    //         console.log('clean up');
-    //         clearTimeout(identifier);  // 새로운 타이머를 설정하기 전 마지막 타이머 삭제
-    //     };
-    // }, [enteredEmail, enteredPassword]);
+        return () => {
+            console.log('clean up');
+            clearTimeout(identifier);  // 새로운 타이머를 설정하기 전 마지막 타이머 삭제
+        };
+    }, [emailIsValid, passwordIsValid]);
 
     const emailChangeHandler = (event) => {
         dispatchEmail({
             type: 'USER_INPUT',
             val: event.target.value, // 페이로드
-
         });
 
         setFormIsValid(
-            event.target.value.includes('@') && enteredPassword.trim().length > 6
+            event.target.value.includes('@') && passwordState.isValid
         );    
     };
 
     const passwordChangeHandler = (event) => {
-        setEnteredPassword(event.target.value);
+        dispatchPassword({type: 'USER_INPUT', val: event.target.value});
 
         setFormIsValid(
             emailState.isValid && event.target.value.trim().length > 6
@@ -70,16 +94,16 @@ const Login = (props) => {
 
     const validateEmailHandler = () => {
         // 추가해야하는 데이터 X -> 페이로드 입력하지 않아도 됨.
-        dispatchEmail({ type: 'INPUT_BLUR' })
+        dispatchEmail({ type: 'INPUT_BLUR' });
     };
 
     const validatePasswordHandler = () => {
-        setPasswordIsValid(enteredPassword.trim().length > 6);
+        dispatchPassword({ type: 'INPUT_BLUR' });
     };
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(emailState.value, enteredPassword);
+        props.onLogin(emailState.value, passwordState.value);
     };
 
     return(
@@ -101,14 +125,14 @@ const Login = (props) => {
                 </div>
                 <div
                     className={`${classes.control} ${
-                        passwordIsValid === false ? classes.invalid : ''
+                        passwordState.isValid === false ? classes.invalid : ''
                     }`}
                 >
                     <label htmlFor="password">Password</label>
                     <input
                         type="password"
                         id="password"
-                        value={enteredPassword}
+                        value={passwordState.value}
                         onChange={passwordChangeHandler}
                         onBlur={validatePasswordHandler}
                     />
