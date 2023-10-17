17e899ca3588accde66c6e9611aaa889c483155f
SO HUI <haejuuun925@gmail.com>
2023-10-17 22:23:28 +0900

    feat: AuthContext 소비자를 useContext 훅으로 변경 후 컨텍스트에 탭핑

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,34 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from './Navigation.module.css';
 import AuthContext from "../../store/auth-context";
 
 const Navigation = (props) => {
+    // 1. 컴포넌트 함수에서 useContext 호출.
+    // 2. 컨텍스트에게 사용하려는 컨텍스트를 가리키는 포인터 전달.
+    const ctx = useContext(AuthContext);
     return(
-        <AuthContext.Consumer>
-            {(ctx) => {
-                return (
-                    <nav className={classes.nav}>
-                        <ul>
-                            {ctx.isLoggedIn && (
-                                <li>
-                                    <a href="/">Users</a>
-                                </li>
-                            )}
-                            {ctx.isLoggedIn && (
-                                <li>
-                                    <a href="/">Admin</a>
-                                </li>
-                            )}
-                            {ctx.isLoggedIn && (
-                                <li>
-                                    <button onClick={props.onLogout}>Logout</button>
-                                </li>
-                            )}
-                        </ul>
-                    </nav>
-                )
-            }}
-        </AuthContext.Consumer>
+        <nav className={classes.nav}>
+            <ul>
+                {ctx.isLoggedIn && (
+                    <li>
+                        <a href="/">Users</a>
+                    </li>
+                )}
+                {ctx.isLoggedIn && (
+                    <li>
+                        <a href="/">Admin</a>
+                    </li>
+                )}
+                {ctx.isLoggedIn && (
+                    <li>
+                        <button onClick={props.onLogout}>Logout</button>
+                    </li>
+                )}
+            </ul>
+        </nav>
     );
 };
 
