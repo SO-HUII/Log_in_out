6f68b92a9059cee0fb0b3ec39c11f74839323a8b
SO HUI <haejuuun925@gmail.com>
2023-10-20 17:51:37 +0900

    refactor: Input 컴포넌트 구현

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card/Card";
 import classes from './Login.module.css';
 import Button from "../UI/Button/Button";
 import AuthContext from "../../store/auth-context";
+import Input from "../UI/Input/Input";
 
 // 컴포넌트 함수 바깥에 생성한 이유
 // because 해당 리듀서 함수 내부에서는 컴포넌트 함수 내부에서 만들어진 어떠한 데이터도 필요하지 않기 때문.
@@ -112,34 +113,24 @@ const Login = (props) => {
     return(
         <Card className={classes.login}>
             <form onSubmit={submitHandler}>
-                <div
-                    className={`${classes.control} ${
-                        emailState.isValid === false ? classes.invalid : ''
-                    }`}
-                >
-                    <label htmlFor="email">E-Mail</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={emailState.value}
-                        onChange={emailChangeHandler}
-                        onBlur={validateEmailHandler}
-                    />
-                </div>
-                <div
-                    className={`${classes.control} ${
-                        passwordState.isValid === false ? classes.invalid : ''
-                    }`}
-                >
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={passwordState.value}
-                        onChange={passwordChangeHandler}
-                        onBlur={validatePasswordHandler}
-                    />
-                </div>
+                <Input 
+                    id="email" 
+                    label="E-Mail" 
+                    type="email" 
+                    isValid={emailIsValid}
+                    value={emailState.value}
+                    onChange={emailChangeHandler}
+                    onBlur={validateEmailHandler}
+                />
+                <Input 
+                    id="password" 
+                    label="Password" 
+                    type="password" 
+                    isValid={passwordIsValid}
+                    value={passwordState.value}
+                    onChange={passwordChangeHandler}
+                    onBlur={validatePasswordHandler}
+                />
                 <div className={classes.actions}>
                     <Button type="submit" className={classes.btn} disabled={!formIsValid}>
                         Login
